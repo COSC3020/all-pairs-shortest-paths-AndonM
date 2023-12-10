f69daf62311058f0b1d671f3c14fc76e3a025ca4
Andon Mefford <andonmefford@outlook.com>
2023-12-10 15:27:36 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,27 +1,79 @@
 const fs = require('fs');
-const jsc = require('jsverify');
+const assert = require('assert');
 
-eval(fs.readFileSync('code.js') + '');
+eval(fs.readFileSync('code.js').toString());
 
-const shortestPathsProperty = jsc.forall(jsc.array(jsc.pair(jsc.nat, jsc.nat)), function (edges) {
-  const max = edges.reduce(function (a, b) {
-    return Math.max(a, Math.max(b[0], b[1]));
-  }, 0);
+function testAllPairsShortestPaths(graph, expected) {
+  const result = allPairsShortestPaths(graph);
+  assert.deepStrictEqual(result, expected, 'Test Failed');
+}
 
-  const mat = [];
-  for (let i = 0; i <= max; i++) {
-    mat[i] = [];
-    for (let j = 0; j <= max; j++) {
-      mat[i][j] = Infinity;
-    }
-    for (const element of edges) {
-      if (element[0] === i) mat[i][element[1]] = 1;
-    }
-  }
-  const graph = mat.map(function(row) {
-    return row.slice();
-  });
-  return allPairsShortestPaths(graph)
-});
+var graph1 = [
+    [0, 2, 5, 8],
+    [7, 0, 4, 6],
+    [9, 1, 0, 3],
+    [7, 9, 8, 0]
+];
+var expected1 = [
+    [0, 2, 5, 3],
+    [1, 0, 4, 2],
+    [3, 1, 0, 3],
+    [1, 3, 2, 0]
+];
+testAllPairsShortestPaths(graph1, expected1);
 
-jsc.assert(shortestPathsProperty, { tests: 1000 });
+var graph2 = [
+    [0, 10, 3, 7],
+    [2, 0, 15, 4],
+    [7, 4, 0, 1],
+    [5, 2, 8, 0]
+];
+var expected2 = [
+    [0, 8, 3, 3],
+    [2, 0, 5, 4],
+    [6, 4, 0, 1],
+    [5, 2, 7, 0]
+];
+testAllPairsShortestPaths(graph2, expected2);
+
+var graph3 = [
+    [0, 5, 1, 9],
+    [2, 0, 7, 3],
+    [6, 4, 0, 1],
+    [8, 2, 4, 0]
+];
+var expected3 = [
+    [0, 3, 1, 3],
+    [2, 0, 4, 3],
+    [3, 1, 0, 1],
+    [5, 2, 4, 0]
+];
+testAllPairsShortestPaths(graph3, expected3);
+
+var graph4 = [
+    [0, 1, 3, 5],
+    [2, 0, Infinity, 4],
+    [Infinity, 4, Infinity, 2],
+    [6, 4, 7, 0]
+];
+var expected4 = [
+    [0, 1, 3, 5],
+    [2, 0, 6, 4],
+    [6, 4, 0, 2],
+    [6, 4, 5, 0]
+];
+testAllPairsShortestPaths(graph4, expected4);
+
+var graph5 = [
+    [0, 8, 1, 9],
+    [2, 0, 7, 3],
+    [6, 4, 0, 1],
+    [8, 2, 4, 0]
+];
+var expected5 = [
+    [0, 6, 1, 3],
+    [2, 0, 4, 3],
+    [3, 1, 0, 1],
+    [5, 2, 4, 0]
+];
+testAllPairsShortestPaths(graph5, expected5);
