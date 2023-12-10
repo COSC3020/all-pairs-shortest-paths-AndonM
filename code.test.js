ddb3857cf8f78f4189f4e2c61da94b1f21b7c384
Andon Mefford <andonmefford@outlook.com>
2023-12-10 16:29:32 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,40 +1,52 @@
+// TESTING SUITE FROM ziyuWang6666
+
 const fs = require('fs');
 const assert = require('assert');
 
 eval(fs.readFileSync('code.js')+'');
 
+// case one
 var graph = [
-  [0, 2, 5],
-  [7, 0, 4],
-  [9, 1, 0]
+    [0, 5, Infinity, 10],
+    [Infinity, 0, 3, Infinity],
+    [Infinity, Infinity, 0, 1],
+    [Infinity, Infinity, Infinity, 0],
 ];
-var expected = [
-  [0, 2, 4],
-  [5, 0, 4],
-  [7, 1, 0]
-];
-assert(JSON.stringify(allPairsShortestPaths(graph)) == JSON.stringify(expected));
+var result = [
+    [ 0, 5, 8, 9 ],
+    [ Infinity, 0, 3, 4 ],
+    [ Infinity, Infinity, 0, 1 ],
+    [ Infinity, Infinity, Infinity, 0 ]
+]
+assert(JSON.stringify(allPairsShortestPaths(graph)) == JSON.stringify(result));
 
+// case two
 var graph = [
-  [0, 10, 3],
-  [2, 0, 15],
-  [7, 4, 0]
+    [1, 4, Infinity, 3],
+    [5, 0, Infinity, Infinity],
+    [6, Infinity, Infinity, Infinity],
+    [3, Infinity, 2, Infinity],
 ];
-var expected = [
-  [0, 10, 3],
-  [2, 0, 5],
-  [7, 4, 0]
-];
-assert(JSON.stringify(allPairsShortestPaths(graph)) == JSON.stringify(expected));
+var result = [
+    [ 1, 4, 5, 3 ],
+    [ 5, 0, 10, 8 ],
+    [ 6, 10, 11, 9 ],
+    [ 3, 7, 2, 6 ]
+]
 
+assert(JSON.stringify(allPairsShortestPaths(graph)) == JSON.stringify(result));
+
+// case three
 var graph = [
-  [0, 5, 1],
-  [2, 0, 7],
-  [6, 4, 0]
+    [Infinity, 5, Infinity, 10],
+    [3, Infinity, 3, Infinity],
+    [Infinity, 4, 0, 1],
+    [7, Infinity, Infinity, 0],
 ];
-var expected = [
-  [0, 5, 1],
-  [2, 0, 3],
-  [6, 4, 0]
-];
-assert(JSON.stringify(allPairsShortestPaths(graph)) == JSON.stringify(expected));
+var result = [
+    [ 8, 5, 8, 9 ],
+    [ 3, 7, 3, 4 ],
+    [ 7, 4, 0, 1 ],
+    [ 7, 12, 15, 0 ]
+]
+assert(JSON.stringify(allPairsShortestPaths(graph)) == JSON.stringify(result));
